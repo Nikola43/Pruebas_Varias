00b96403eada33eacbc75433d32ce66c65c8a71c
Paulo Soares <pauloxti@gmail.com>
2018-11-20 14:51:51 +0100

    generando clave

--- app.js
+++ app.js
@@ -1,17 +1,58 @@
-var str = ['0','x'];
+var str = [];
 
 function initializeKey() {
     for (let i = 0; i < 64; i++) {
-        str.push('0')
+        str.push(0)
     }
 }
 
-function printKey(key) {
+function printPrivateKey(key) {
     for (let i = 0; i < key.length; i++) {
-        process.stdout.write(key[i]);
+        process.stdout.write(key[i].toString());
     }
+    console.log();
 }
 
 initializeKey();
-printKey(str);
+printPrivateKey(str);
+incrementPrivateKey(str);
+incrementPrivateKey(str);
+incrementPrivateKey(str);
+incrementPrivateKey(str);
+incrementPrivateKey(str);
+incrementPrivateKey(str);
+incrementPrivateKey(str);
+incrementPrivateKey(str);
+incrementPrivateKey(str);
+incrementPrivateKey(str);
+incrementPrivateKey(str);
+incrementPrivateKey(str);
+incrementPrivateKey(str);
+incrementPrivateKey(str);
+incrementPrivateKey(str);
+incrementPrivateKey(str);
+incrementPrivateKey(str);
+incrementPrivateKey(str);
+incrementPrivateKey(str);
+incrementPrivateKey(str);
 
+
+function incrementPrivateKey(privateKey) {
+    var keyLength = privateKey.length;
+
+    if (privateKey[keyLength - 1] !== 'F'){
+
+
+        privateKey[keyLength - 1]++;
+
+        switch (privateKey[keyLength - 1]) {
+            case 10  : privateKey[keyLength - 1] = 'A'; break;
+            case 'A' : privateKey[keyLength - 1] = 'B'; break;
+            case 'B' : privateKey[keyLength - 1] = 'C'; break;
+            case 'C' : privateKey[keyLength - 1] = 'D'; break;
+            case 'D' : privateKey[keyLength - 1] = 'E'; break;
+            case 'E' : privateKey[keyLength - 1] = 'F'; break;
+        }
+        printPrivateKey(privateKey);
+    }
+}
